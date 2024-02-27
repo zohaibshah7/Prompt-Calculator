4ed090a3b2bf4c60f9cd5fa3da12f6da85cdb1fc
Syed Zohaib Shah <154647656+zohaibshah7@users.noreply.github.com>
2024-02-27 11:53:43 +0500

    calculator

---
+++ app.js
@@ -0,0 +1,21 @@
+let userName = prompt("What is Your Name");
+alert("Welcome " + userName);
+console.log("Welcome " + userName);
+
+let a = parseFloat(prompt("Enter the Value of a"));
+let b = parseFloat(prompt("Enter the value of b"));
+
+let Sum = a + b ;
+let Diff = a - b ;
+let Mul = a * b ;
+let Div = a / b ;
+
+alert("Sum of a, b, is " + Sum);
+alert("Diff of a, b, is " + Diff);
+alert("Mul of a, b,  is " + Mul);
+alert("Div of a, b,  is " + Div);
+
+console.log("Sum of a, b, is " + Sum);
+console.log("Diff of a, b,  is " + Diff);
+console.log("Mul of a, b, " + Mul);
+console.log("Div of a, b,  is " + Div);
